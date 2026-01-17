30dcab8d1f807276dfc38426c5d5861654285765
최동호 <raspic5874@naver.com>
2026-01-17 14:10:21 +0900

    [Feat] 멤버 조회

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -25,4 +25,6 @@ public class Member {
     public void changeUsername(String username) {
         this.username = username;
     }
+
+
 }
--- src/main/java/com/example/demo/web/controller/MemberController.java
+++ src/main/java/com/example/demo/web/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.demo.web.controller;
 import com.example.demo.service.MemberService;
 import com.example.demo.web.dto.MemberCreateRequest;
 import com.example.demo.web.dto.MemberCreateResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,5 +17,11 @@ public class MemberController {
     @PostMapping("/create")
     public MemberCreateResponse create(@RequestBody MemberCreateRequest request) {
         return memberService.create(request);
+    }
+
+    @GetMapping("/{memberid}")
+    public String getMember(@PathVariable("memberid") Long memberId) {
+        // This is a placeholder implementation.
+        return "Member ID: " + memberId;
     }
 }
