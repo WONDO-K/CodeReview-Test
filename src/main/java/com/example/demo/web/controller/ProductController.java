494122ea272dd2c8b8d42a2c122f94965bec8e5f
최동호 <raspic5874@naver.com>
2026-02-01 20:51:30 +0900

    [Feat] 제품 상세

--- src/main/java/com/example/demo/web/controller/ProductController.java
+++ src/main/java/com/example/demo/web/controller/ProductController.java
@@ -1,11 +1,14 @@
 // web/ProductController.java
 package com.example.demo.web.controller;
 
+import com.example.demo.domain.Product;
 import com.example.demo.service.ProductService;
 import com.example.demo.web.dto.ProductCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +26,10 @@ public class ProductController {
     public ResponseEntity<Void> delete(@PathVariable Long productId) {
         productService.delete(productId);
         return ResponseEntity.noContent().build(); // 요청 성공, 클라이언트에게 내려줄 응답바디는 없다.
+    }
+
+    @GetMapping("/{productId}")
+    public Optional<Product> getProduct(@PathVariable Long productId) {
+        return productService.getProduct(productId);
     }
 }
